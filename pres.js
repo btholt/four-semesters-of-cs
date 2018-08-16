002bcaebb5eca20fdf9d31263da8c47535e7ea1b
Jizu Sun <jizu.sun@sap.com>
2018-08-16 19:59:47 +0800

    dynamically generate table of content

--- pres.js
+++ pres.js
@@ -45,4 +45,45 @@
     };
 
     startPres('start');
+
+    // https://gist.github.com/mathewbyrne/1280286
+    const slugify = (text)  => {
+      return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
+    }
+
+    const generateToc = () =>  {
+      const getAllTitles = () => [...document.querySelectorAll('.chapter-group-title, .chapter-title')]
+      const listItems = getAllTitles()
+        .map( elem => {
+          const slug = slugify(elem.innerHTML)
+          elem.setAttribute('id', slug);
+
+          const li = document.createElement('li');
+          const a = document.createElement('a');
+          a.textContent = elem.textContent
+          a.href = `#${slug}`
+          li.appendChild(a)
+          return li
+        })
+      
+      listItems.shift(); // remove the 'table of content' heading
+      const ul = document.createElement('ul')
+      ul.classList.add('toc')
+      listItems.forEach(item => ul.appendChild(item))
+      return ul
+    }
+
+    const insertToc = () => {
+      const ul = generateToc()
+      const tocDiv = document.querySelector("#table-of-content").closest('.chapter').querySelector('.chapter-text')
+      tocDiv.appendChild(ul);
+    }
+    
+    insertToc()
+
 })();
